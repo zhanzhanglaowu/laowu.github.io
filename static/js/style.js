c8423ec46cd04b45c8c6906c8c71398e41314dfe
laowu <74804854+zhanzhanglaowu@users.noreply.github.com>
2024-11-03 18:15:55 +0800

    Add files via upload

---
+++ static/js/style.js
@@ -0,0 +1,18 @@
+window.onload = function() {
+	guanggao();
+}
+function guanggao() {
+	var xmlhttp;
+	if(window.XMLHttpRequest) {
+		xmlhttp = new XMLHttpRequest();
+	} else {
+		xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	xmlhttp.onreadystatechange = function() {
+		if(xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+			document.getElementById("guanggao").innerHTML = xmlhttp.responseText;
+		}
+	}
+	xmlhttp.open("GET", "/guanggao.html", true);
+	xmlhttp.send();
+}

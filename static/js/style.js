57db84c24341575adbf9aa3e48e9f071f6308ed9
laowu <74804854+zhanzhanglaowu@users.noreply.github.com>
2024-11-08 23:42:36 +0800

    Update style.js

--- static/js/style.js
+++ static/js/style.js
@@ -11,6 +11,7 @@ function guanggao() {
 	xmlhttp.onreadystatechange = function() {
 		if(xmlhttp.readyState == 4 && xmlhttp.status == 200) {
 			document.getElementById("guanggao").innerHTML = xmlhttp.responseText;
+			translate.execute();
 		}
 	}
 	xmlhttp.open("GET", "/guanggao.html", true);
